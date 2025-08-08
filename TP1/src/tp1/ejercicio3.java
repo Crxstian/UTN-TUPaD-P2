7967ac570ed5af528a68c93392237df0294fc585
crxstian <cristian19overdrive@gmail.com>
2025-08-08 20:26:23 -0300

    Subiendo TP1

---
+++ TP1/src/tp1/ejercicio3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1;
+
+/**
+ *
+ * @author Cristian
+ */
+public class ejercicio3 {
+    
+    String nombre = "Cristian";
+    int edad = 29;
+    double altura = 1.74;
+    boolean estudiante = true;
+        
+    
+}
