9b84f524bc394332c1242718528dbeb627ba0d35
crxstian <cristian19overdrive@gmail.com>
2025-08-19 21:11:06 -0300

    Subiendo Resolucion TP2

--- TP1/src/tp1/TP1.java
+++ TP1/src/tp1/TP1.java
@@ -15,7 +15,11 @@ public class TP1 {
     /**
      * @param args the command line arguments
      */
+    static double precioBase;
+    static final double descuento = 0.10;
+
     public static void main(String[] args) {
+        
         
 //        System.out.println("Hola mundo");
 //
@@ -26,7 +30,7 @@ public class TP1 {
 //        
 //        System.out.println("Hola mi nombre es "+nombre+" tengo "+edad+" anios de edad, mido "+altura+" metros y mi condicion de estudiante es: "+estudiante);
 //        
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 //        String nombre;
 //        int edad;
 //        
@@ -75,28 +79,308 @@ public class TP1 {
     *
     * Ejercicio 8
     */
-        double numero1,numero2,division;
-        System.out.println("Por favor ingrese un numero: ");
-        numero1 = scanner.nextInt();
-        
-        System.out.println("Por favor ingrese otro numero: ");
-        numero2 = scanner.nextInt();
-        
-        division = numero1 / numero2;
-        System.out.println("El resultado para la operacion de division entre "+numero1+" y "+numero2+" es: "+division);
+//        double numero1,numero2,division;
+//        System.out.println("Por favor ingrese un numero: ");
+//        numero1 = scanner.nextInt();
+//        
+//        System.out.println("Por favor ingrese otro numero: ");
+//        numero2 = scanner.nextInt();
+//        
+//        division = numero1 / numero2;
+//        System.out.println("El resultado para la operacion de division entre "+numero1+" y "+numero2+" es: "+division);
+//        
+//    /**
+//    *
+//    * Ejercicio 10
+//    */
+//        
+//
+//        int a = 5;
+//        int b = 2;
+//        int resultado = a / b;
+//        System.out.println("Resultado: " + resultado);
+//   
+    /**
+    * @author Cristian
+    * 1. Verificación de Año Bisiesto. 
+    */
+    
+//    Scanner scanner = new Scanner(System.in);
+//    int anio;
+//    System.out.println("Ingrese un año: ");
+//    anio = scanner.nextInt();
+//    
+//        if ((anio%4 ==0)&&(anio%100!=0)||(anio%400 == 0)) {
+//            System.out.println("El año "+anio+" es bisiesto.");
+//        }
+//        else{
+//            System.out.println("El año "+anio+" no es bisiesto.");
+//        }   
+
+    /**
+    * @author Cristian
+    * 2. Determinar el Mayor de Tres Números.
+    */
+//    Scanner scanner = new Scanner(System.in);
+//    int entero1,entero2,entero3,mayor;
+//    
+//    System.out.println("Ingrese un numero: ");
+//    entero1 = scanner.nextInt();
+//    System.out.println("Ingrese otro numero: ");
+//    entero2 = scanner.nextInt();
+//    System.out.println("Ingrese un tercer numero: ");
+//    entero3 = scanner.nextInt();
+//    
+//    mayor = entero1;
+//    
+//        if (entero2>mayor) {
+//            mayor = entero2;
+//        }
+//        if (entero3>mayor) {
+//            mayor = entero3;
+//        }
+//        System.out.println("El mayor es: "+mayor);
         
     /**
-    *
-    * Ejercicio 10
-    */
+    * @author Cristian
+    * 3. Clasificación de Edad. 
+    */       
+//    Scanner scanner = new Scanner(System.in);
+//    int edad;
+//    System.out.println("Ingrese su edad: ");
+//    edad = scanner.nextInt();
+//    
+//        if (edad<12) {
+//            System.out.println("Eres un Niño.");
+//        }else if (edad>=12 && edad<=17) {
+//            System.out.println("Eres un Adolescente.");
+//        }else if (edad>=18 && edad <=59) {
+//            System.out.println("Eres un Adulto.");
+//        }else{
+//            System.out.println("Eres un Adulto Mayor.");
+//        }
+    /**
+    * @author Cristian
+    * 4. Calculadora de Descuento según categoría. 
+    */         
+//    Scanner scanner = new Scanner(System.in);
+//    double precio,descuento,precioFinal;
+//    String categoria;
+//    System.out.println("Ingrese el precio del producto: ");
+//    precio = scanner.nextInt();
+//    scanner.nextLine(); 
+//    
+//    System.out.println("Ingrese la categoria del producto (A, B o C): ");
+//    categoria = scanner.nextLine().toUpperCase();
+//    
+//        switch (categoria) {
+//            case "A":
+//                System.out.println("Descuento aplicado: 10%");
+//                descuento = 0.10;
+//                break;
+//            case "B":
+//                System.out.println("Descuento aplicado: 15%");
+//                descuento = 0.15;
+//                break;
+//            case "C":
+//                System.out.println("Descuento aplicado: 20%");
+//                descuento = 0.20;
+//            default:
+//                System.out.println("Categoría inválida., Debe ingresar una de las opciones indicadas");
+//                return;
+//        }
+//    precioFinal = precio - (precio*descuento);
+//        System.out.println("Precio final: "+precioFinal);
+    
+    
+    /**
+    * @author Cristian
+    * 5. Suma de Numeros Pares (while)
+    */   
+    
+//    Scanner scanner = new Scanner(System.in);
+//    int numero,suma =0;
+//
+//    
+//        do {
+//            System.out.println("Ingrese un número (0 para terminar): ");
+//            numero = scanner.nextInt();
+//            if (numero%2 ==0) {
+//            suma += numero;
+//            }
+//        } while (numero!=0);
+//        System.out.println("La suma de los números pares es: "+suma);
         
+        /**
+    * @author Cristian
+    * 6. Contador de positivos, Negativos y Ceros(for)
+    */   
+    
+//    Scanner scanner = new Scanner(System.in);
+//    int numero,positivos =0,negativos=0,ceros=0;
+//    
+//    
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("Ingrese el número "+i+": ");
+//            numero = scanner.nextInt();
+//            if (numero>0) {
+//                positivos ++;
+//            }else if(numero==0){
+//                ceros ++;
+//            }
+//            else{
+//                negativos ++;
+//            }
+//        }
+//        System.out.println("Resultados: ");
+//        System.out.println("Positivos: "+positivos);
+//        System.out.println("Negativos: "+negativos);
+//        System.out.println("Ceros: "+ceros);
+    
+    /**
+    * @author Cristian
+    * 7. Validacion de Nota entre 0 y 10 (do-while)
+    */       
+        
+//    Scanner scanner = new Scanner(System.in);
+//    int nota;
+//        do {            
+//            System.out.println("Ingrese una nota (0-10): ");
+//            nota = scanner.nextInt();
+//            if (nota<0 || nota>10) {
+//                System.out.println("Error: Nota invalida. Ingrese una nota entre 0 y 10.");
+//            }
+//        } while (nota<0 || nota>10);
+//        System.out.println("Nota guardad correctamente.");
 
-        int a = 5;
-        int b = 2;
-        int resultado = a / b;
-        System.out.println("Resultado: " + resultado);
+    /**
+    * @author Cristian
+    * 8. Validacion de Nota entre 0 y 10 (do-while)
+    */       
+//    Scanner scanner = new Scanner(System.in);
+//    double impuestoPorcentaje,descuentoPorcentaje;
+//    
+//    System.out.print("Ingrese el precio base del producto: ");
+//    precioBase = scanner.nextDouble();
+//    System.out.print("Ingrese el impuesto en porcentaje (Ejemplo: 10 para 10%): ");
+//    impuestoPorcentaje = scanner.nextDouble()/100;
+//    System.out.print("Ingrese el descuento en porcentaje(Ejemplo: 5 para 5%: ");
+//    descuentoPorcentaje = scanner.nextDouble()/100;
+//    
+//    System.out.println("El precio final del producto es: "+calcularPrecioFinal(impuestoPorcentaje,descuentoPorcentaje));
+    
+    
+    /**
+    * @author Cristian
+    * 9. Composicion de funciones para calcular costo de envio y total de compra
+    */  
+//    Scanner scanner = new Scanner(System.in);
+//    double precioProducto,pesoKg;
+//    String zonaEnvio;
+//    System.out.print("Ingrese el precio del producto: ");
+//    precioProducto = scanner.nextDouble();
+//    System.out.print("Ingrese el peso del paquete en kg: ");
+//    pesoKg = scanner.nextDouble();
+//    scanner.nextLine();
+//    System.out.println("Ingrese la zona de envio (Nacional/Internacional): ");
+//    zonaEnvio = scanner.nextLine().toUpperCase();
+//    
+//    System.out.println("El costo de envio es: "+calcularCostoEnvio(pesoKg, zonaEnvio));
+//    System.out.println("El total a pagar es: "+calcularTotalCompra(precioProducto, calcularCostoEnvio(pesoKg, zonaEnvio)));
     
 
+    /**
+    * @author Cristian
+    * 10. Actualización de stock a partir de venta y recepción de productos.
+    */ 
+    
+//    Scanner scanner = new Scanner(System.in);
+//    int stockActual,cantidadVendida,cantidadRecibida;
+//    
+//    System.out.println("Ingrese el stock actual del producto: ");
+//    stockActual = scanner.nextInt();
+//    System.out.println("Ingrese la cantidad vendida: ");
+//    cantidadVendida = scanner.nextInt();
+//    System.out.println("Ingrese la cantidad recibida: ");
+//    cantidadRecibida = scanner.nextInt();
+//    
+//    System.out.println("El nuevo stock del producto es: "+actualizarStock(stockActual, cantidadVendida, cantidadRecibida));
+    
+    /**
+    * @author Cristian
+    * 11. Cálculo de descuento especial usando variable global. 
+    */  
+//    Scanner scanner = new Scanner(System.in);
+//    double precioProducto;
+//    
+//    System.out.println("Ingrese el precio del producto: ");
+//    precioProducto = scanner.nextDouble();
+//    calcularDescuentoEspecial(precioProducto);
+    
+    
+    
+    /**
+    * @author Cristian
+    * 12. Modificación de un array de precios y visualización de resultados.  
+    */  
+//    double[] precios = {199.99,299.5,149.75,399.0,89.99};
+//    System.out.println("Precios Origniales:");
+//        for (double precio : precios) {
+//            System.out.println("Precio: $"+precio);
+//        }
+//    precios[2]=129.99;
+//    System.out.println("Precios Modificados:");
+//    for (double precio : precios) {
+//        System.out.println("Precio: $"+precio);
+        
+    /**
+    * @author Cristian
+    * 13. Impresión recursiva de arrays antes y después de modificar un elemento. 
+    */          
+        double[] precios = {199.99, 299.5, 149.75, 399.0, 89.99};
+
+        System.out.println("Precios originales:");
+        imprimirRecursivo(precios, 0);
+
+        precios[2] = 129.99;
+
+        System.out.println("Precios modificados:");
+
+        imprimirRecursivo(precios, 0);    
+        
     }
     
+    public static double calcularPrecioFinal(double impuesto,double descuento){
+        double precioFinal = precioBase + (precioBase * impuesto) - (precioBase * descuento);
+        return precioFinal;
+    }
+    public static double calcularCostoEnvio(double peso,String zona){
+        double costoEnvio =0;
+        if (zona.equalsIgnoreCase("Nacional")) {
+            costoEnvio = peso*5;
+        }else if(zona.equalsIgnoreCase("Internacional")){
+            costoEnvio =  peso*10;
+        }
+        return costoEnvio;
+    }
+    public static double calcularTotalCompra(double  precioProducto,double costoEnvio){
+        return precioProducto + costoEnvio;
+    }
+    
+    public static int actualizarStock(int stockActual,int cantidadVendida, int cantidadRecibida){
+        return stockActual - cantidadVendida +cantidadRecibida;
+    }
+    
+    public static  void calcularDescuentoEspecial(double precio){
+        double descuentoAplicado;
+        descuentoAplicado = precio*descuento;
+        System.out.println("El descuento especial aplicado es: "+descuentoAplicado);
+        System.out.println("El precio final con descuento es: "+(precio-descuentoAplicado));
+    }
+    public static void imprimirRecursivo(double[] array,int indice){
+            if (indice < array.length) {
+                System.out.println("Precio: $" + array[indice]);
+                imprimirRecursivo(array, indice + 1);
+            }
+    }
 }
---
+++ TP1/src/tp1/TP2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cristian
+ * 1. Verificación de Año Bisiesto. 
+Escribe un programa en Java que solicite al usuario un año y determine si es 
+bisiesto. Un año es bisiesto si es divisible por 4, pero no por 100, salvo que sea 
+divisible por 400. 
+Ejemplo de entrada/salida: 
+Ingrese un año: 2024 
+El año 2024 es bisiesto. 
+Ingrese un año: 1900 
+El año 1900 no es bisiesto. 
+ */
+public class TP2 {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+
+    
+}
